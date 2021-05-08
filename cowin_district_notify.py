fd9113f26ffae2ffe8115a6ff7c58b809fc7ee8e
kondorpa <kondorpa.borchetia@gmail.com>
2021-05-08 14:26:43 +0530

    Update cowin_district_notify.py

--- cowin_district_notify.py
+++ cowin_district_notify.py
@@ -85,9 +85,6 @@ while True:
 		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Safari/605.1.15',
 		'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0',
 		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-		'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0',
-		'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
-		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36",
 		]
 		
 		#Pick a random user agent
@@ -140,7 +137,7 @@ while True:
 	 #       message = client.messages \
 	 #           .create(
 	 #           body=sms_text,
-	 #           from_='+17573376693', #Twilio  configured mobile number
+	 #           from_='+1xxxxxxx', #Twilio  configured mobile number
 	 #           to='+91xxxxxxxxxx' #Mobile number that needs to be notified, your personal number
 	 #       )
 	 
@@ -171,7 +168,7 @@ while True:
 		    msg_body = whatsapp_df.to_string(index = False)
 		    for number in num:
 			message = client.messages.create( 
-				    from_='whatsapp:+14155238886',  
+				    from_='whatsapp:+14155xxxxxx',  
 				    body=msg_body,      
 				    to=number 
 				)  
